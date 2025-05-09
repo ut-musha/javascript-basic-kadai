95068bb90abd7d4a51a8018b505d780c9c78877b
ut-musha <ut.musha@gmail.com>
2025-05-09 17:37:24 +0900

    Add files via upload

---
+++ function.js
@@ -0,0 +1,53 @@
+// const  sayGoodMorning  =()=>{
+//     console.log('おはようございます！');
+//     console.log('昨日はよく眠れましたか？');
+//     console.log('今日も一日がんばりましょう！');
+// }
+
+// const  sayGoodEvening  =()=>{
+//     console.log("こんばんは！");
+//     console.log('今日も１日お疲れ様でした！');
+// }
+
+// sayGoodMorning();
+
+// sayGoodEvening();
+
+// const calculateTotal = (price)  =>{
+//     console.log(price+500+'円');
+// }
+
+// calculateTotal(1200);
+
+// const addTwoArguments=(price,shippingFee)=>{
+//     console.log(price+shippingFee+'円');
+// }
+// addTwoArguments(1000,500);
+
+
+
+// const double=(num)=>{
+//     return num*2;
+// }
+// console.log(double(30));
+
+
+
+// const useVariable   =()=>{
+//     const userName='侍太郎'
+//     console.log(userName);
+// }
+
+// useVariable();
+// console.log(userName);
+
+
+const callName =(name )=>{
+    return('いらっしゃいませ'+  (name)+'さん!');
+}
+
+const shoppingSum =(num,price)=>{
+    return price * num ;
+}
+console.log(callName('UTA'));
+console.log(shoppingSum(3,100));
